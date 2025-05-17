9fdbcfd0f1d85eb707e5c3b834b403793681a50f
Ru-kko <verase2003.1@gmail.com>
2025-05-17 03:57:29 -0500

    style: remove unused imports

--- App/app/src/main/java/com/stayflow/app/ui/components/NavHost.kt
+++ App/app/src/main/java/com/stayflow/app/ui/components/NavHost.kt
@@ -8,13 +8,9 @@ import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
